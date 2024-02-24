8b9567df4369a26118b075548c8fc0f3be9b690f
Víctor Carrasco <vc.business.oo@gmail.com>
2024-02-24 14:53:13 +0100

    feat(initial)

---
+++ project_manager/settings/production.py
@@ -0,0 +1,16 @@
+from .base import *
+
+# Database
+# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/5.0/howto/static-files/
+
+STATIC_URL = 'static/'
